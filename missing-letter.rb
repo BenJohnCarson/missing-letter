13e10e1364f97579022a4582ad8a360a0b205b50
Benjamin Carson <benjohncarson@gmail.com>
2016-12-12 15:02:12 +0000

    Passes all tests

--- missing-letter.rb
+++ missing-letter.rb
@@ -1,10 +1,8 @@
 def find_missing_letter(arr)
-	array = arr.sort.map { |c| c.downcase }
-
-	array.each.with_index do |c, i|
+	arr.sort.each.with_index do |c, i|
 		if i != 0
-			if c != array[i-1].succ
-				return array[i-1].succ
+			if c != arr[i-1].succ
+				return arr[i-1].succ
 			end
 		end
 	end
