44e58b2d1fe886784145e4a5d67e1b660823a4d2
Benjamin Carson <benjohncarson@gmail.com>
2016-12-12 15:10:23 +0000

    Refactored to one line

--- missing-letter.rb
+++ missing-letter.rb
@@ -1,11 +1,5 @@
 def find_missing_letter(arr)
-	arr.sort.each.with_index do |c, i|
-		if i != 0
-			if c != arr[i-1].succ
-				return arr[i-1].succ
-			end
-		end
-	end
+	arr.sort.each.with_index { |c, i| return c.succ if c.succ != arr[i+1] }
 end
 
 puts find_missing_letter(["Q","R","S","O"])
